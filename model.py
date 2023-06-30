e975f244acd899c1dc860362a05f390d31fda953
PrakhovEgor <93534247+PrakhovEgor@users.noreply.github.com>
2023-06-30 11:35:07 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,5 @@
 import pickle
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from pickle import dump, load
-import pandas as pd
 
 def model_prediction(df):
     with open('data/model.pickle', 'rb') as f:
